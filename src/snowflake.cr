52d58213797b6f56297756279edad52f77e60d84
Hampton Moore <me@hampton.pw>
2021-10-26 06:57:38 -0400

    Allow setting epoch with Int64s

--- src/snowflake.cr
+++ src/snowflake.cr
@@ -6,7 +6,7 @@ class Snowflake
     @sequence_bits : Int64
     @sequence_max : Int64
     @newepoch : Int64
-    def initialize(@instance : Int64, ep : Time)
+    def initialize(@instance : Int64, ep : Int64)
         @time_bits = 41_i64
         @instance_bits = 10_i64
         if @instance >= 2_i64**@instance_bits
@@ -14,8 +14,12 @@ class Snowflake
         end
         @sequence_bits = 12_i64
         @sequence_max  = 2_i64**@sequence_bits
-        @newepoch = ep.to_unix_ms
+        @newepoch = ep
         @sequence = 0
+    end
+
+    def initialize(@instance : Int64, ep : Time)
+        initialize(@instance, ep.to_unix_ms)
     end
     
     def next
@@ -30,5 +34,13 @@ class Snowflake
         snowflake_id |= @instance << @sequence_bits
         snowflake_id |= @sequence
         snowflake_id
+    end
+
+    def set_epoch(ep : Time)
+        @newepoch = ep.to_unix_ms
+    end
+
+    def set_epoch(ep : Int64)
+        @newepoch = ep
     end
 end
